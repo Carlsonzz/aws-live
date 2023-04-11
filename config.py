68e627ff9496759d3110214d2cd93193945972f1
Uni3uez <130440283+Uni3uez@users.noreply.github.com>
2023-04-11 15:13:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cpjlh1ku47ni.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "carlsonchiew-employee"
+customregion = "us-east-1"
